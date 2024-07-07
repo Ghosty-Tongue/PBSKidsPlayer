fe3d87d6f0c362a96b9645f1761f60cf2d167646
GhostyTongue <50715350+Ghosty-Tongue@users.noreply.github.com>
2024-07-07 02:25:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PBSKidsPlayer',
-    version='0.1.0',
-    description='A package to fetch and play PBS Kids content',
-    author='GhostyTongue'
+    name='pbskidsplayer',
+    version='0.1',
     packages=find_packages(),
     install_requires=[
         'requests',
+        # Add other dependencies here
     ],
-    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pbskidsplayer = pbskidsplayer.main:main',
+        ],
+    },
+    author='Ghosty-Tongue',
+    author_email='your.email@example.com',
+    description='Python Package to fetch PBS Kids Content.',
+    url='https://github.com/Ghosty-Tongue/pbskidsplayer',
 )
